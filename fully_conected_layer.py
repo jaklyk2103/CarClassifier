3928b7c4a11cbe4f3da01ffe08ae86301d4a19f9
tomaszkus97 <tomi88d@interia.pl>
2019-05-22 10:26:26 +0200

    added cnn layers functions

---
+++ fully_conected_layer.py
@@ -0,0 +1,17 @@
+
+#ostatnia warstwa sieci neuronowej, korzysta z funkcji aktywacji relu oraz softmax 
+# (softmax dletego ze mamy wiele klas obrazow), zwraca y czyli wektor predykcji
+def fully_connected_layer(input_data,input_shape):
+    flattened = tf.reshape(input_data, [-1, input_shape[0] * input_shape[1] * input_shape[2]])
+
+    weights_1 = tf.Variable(tf.truncated_normal([input_shape[0] * input_shape[1] * input_shape[2]], 1000], stddev=0.03))
+    biases_1 = tf.Variable(tf.truncated_normal([1000], stddev=0.01))
+    dense_layer1 = tf.matmul(flattened, weights_1) + biases_1
+    dense_layer1 = tf.nn.relu(dense_layer1)
+
+    weights_2 = tf.Variable(tf.truncated_normal([1000, 10], stddev=0.03), name='wd2')
+    biases_2 = tf.Variable(tf.truncated_normal([10], stddev=0.01), name='bd2')
+    dense_layer2 = tf.matmul(dense_layer1, weights_2) + biases_2
+    y = tf.nn.softmax(dense_layer2)
+
+    return y
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from data.data_loader import DataLoader
+from convolution_functions import convolution_layer
 import tensorflow as tf
+import numpy as np
 
 #wczytywanie danych
 classes_path = r'C:\Users\tomi8\Desktop\Dataset\devkit\cars_meta.mat'
@@ -7,13 +9,33 @@ training_annotations  = r'C:\Users\tomi8\Desktop\Dataset\devkit\cars_train_annos
 training_images = r'C:\Users\tomi8\Desktop\Dataset\cars_train'
 
 training_data_loader = DataLoader(classes_path,training_annotations,training_images)
-dataset=training_data_loader.getDataset()
+images=training_data_loader.getDataset()
 #dataset - zawiera pary (zdjecie, id klasy)
-iter = dataset.make_one_shot_iterator()
-el = iter.get_next()
-print(el)
+
+
+#network hyperparameters -> beda zmieniane w procesie "doskonalenia" sieci
+
+learning_rate = 0.001
+training_iters = 200000
+batch_size = 128
+
+classes_number = 196
+input_number = 192 #????? musimy wybrac rozmiar zdjec 
+
+x = tf.placeholder("float", [None, 192,192,1])
+y = tf.placeholder("float", [None, classes_number])
+
+x = tf.reshape(x, [-1, 192, 192, 3])
+
+result = convolution_layer(x,3,[2,2],1)
+
+
+
 
 with tf.Session() as sess:
     init = tf.global_variables_initializer()
     sess.run(init)
+
+    
+    
  
