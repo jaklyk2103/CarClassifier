0a746fa92a0e10850f5abdc43d972bfb9b0fd4a1
Kuba <kubeckk21@gmail.com>
2019-08-22 23:44:13 +0200

    removed unnecessary code from cameraScreen

--- mobileApp/screens/CameraScreen.js
+++ mobileApp/screens/CameraScreen.js
@@ -1,86 +1,9 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Alert,
-  PermissionsAndroid,
-  Platform,
-} from 'react-native';
 import { CameraKitCameraScreen } from 'react-native-camera-kit';
  
 export default class CameraScreen extends React.Component {
   constructor(props) {
     super(props);
-  }
-
-  onPress() {
-    if (Platform.OS === 'android') {
-      async function requestCameraPermission() {
-        try {
-          const permissionResult = await PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.CAMERA,
-            {
-              title: 'CarClassifier App Camera Permission',
-              message: 'CarClassifier App needs access to your camera',
-            }
-          );
-          if (permissionResult === PermissionsAndroid.RESULTS.GRANTED) {
-            requestExternalWritePermission();
-          } else {
-            alert('Camera permission denied, application cannot use camera.');
-          }
-        } catch (err) {
-          alert('Camera permission err', err);
-          console.warn(err);
-        }
-      }
-
-      async function requestExternalWritePermission() {
-        try {
-          const permissionResult = await PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-            {
-              title: 'CarClassifier App External Storage Write Permission',
-              message:
-                'CarClassifier App needs access to Storage data in your SD Card',
-            }
-          );
-          if (permissionResult === PermissionsAndroid.RESULTS.GRANTED) {
-            requestExternalReadPermission();
-          } else {
-            alert('WRITE_EXTERNAL_STORAGE permission denied');
-          }
-        } catch (err) {
-          alert('Write permission err', err);
-          console.warn(err);
-        }
-      }
-
-      async function requestExternalReadPermission() {
-        try {
-          const permissionResult = await PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
-            {
-              title: 'CarClassifier App Read Storage Read Permission',
-              message: 'CarClassifier App needs access to your SD Card',
-            }
-          );
-          if (permissionResult === PermissionsAndroid.RESULTS.GRANTED) {
-            // all permissions granted
-            this.setState({ isPermitted: true });
-          } else {
-            alert('READ_EXTERNAL_STORAGE permission denied');
-          }
-        } catch (err) {
-          alert('Read permission err', err);
-          console.warn(err);
-        }
-      }
-
-      requestCameraPermission();
-    } else {
-      alert('Not an Android device. Application cannot be used.');
-      this.setState({ isPermitted: false });
-    }
   }
 
   onBottomButtonPressed(event) {
@@ -96,11 +19,9 @@ export default class CameraScreen extends React.Component {
   render() {
       return (
       <CameraKitCameraScreen
-          // Buttons to perform action done and cancel
           actions={{ rightButtonText: 'Classify', leftButtonText: 'Cancel' }}
           onBottomButtonPressed={event => this.onBottomButtonPressed(event)}
           flashImages={{
-          // Flash button images
           off: require('../assets/flashoff.png'),
           on: require('../assets/flashon.png'),
           auto: require('../assets/flashauto.png'),
